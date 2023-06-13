5c63054eab1db162b073b918d8ab66babc36a4c2
Junting <342766475@qq.com>
2023-06-13 11:23:08 +0800

    feat: eslint config basic

---
+++ packages/basic/index.js
@@ -0,0 +1,405 @@
+module.exports = {
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+  },
+  // 报告所有未使用的 eslint-disable 指令
+  reportUnusedDisableDirectives: true,
+  extends: [
+    "./standard",
+    "plugin:import/recommended",
+    // 对指令的检验，e.g. /* eslint-disable */
+    "plugin:eslint-comments/recommended",
+    "plugin:jsonc/recommended-with-jsonc",
+    "plugin:markdown/recommended",
+    "plugin:yml/standard"
+  ],
+  ignorePatterns: [
+    "*.min.*",
+    "*.d.ts",
+    "dist",
+    "public",
+    "output",
+    "out",
+    "temp",
+    "coverage",
+    "CHANGELOG.md",
+    "LICENSE*",
+    "package-lock.json",
+    "yarn.lock",
+    "pnpm-lock.yaml",
+    "__snapshots__",
+    // ignore for in lint-staged
+    "*.css",
+    "*.png",
+    "*.ico",
+    "*.toml",
+    "*.patch",
+    "*.txt",
+    "*.crt",
+    "*.key",
+    "Dockerfile",
+    // force exclude
+    ".vitepress/cache",
+    // force include
+    "!.github",
+    "!.vitepress",
+    "!.vscode"
+  ],
+  plugins: [
+    "html",
+    "unicorn",
+    "no-only-tests",
+    "unused-imports",
+  ],
+  // 共享设置
+  settings: {
+    "import/resolver": {
+      node: { extensions: [".js", ".mjs"] },
+    }
+  },
+  overrides: [
+    {
+      files: ["*.json", "*.json5", "*.jsonc"],
+      parser: "json-eslint-parser",
+      rules: {
+        // Disallow or enforce spaces inside of bracket
+        "jsonc/array-bracket-spacing": ["error", "never"],
+        // Require or disallow trailing commas
+        "jsonc/comma-dangle": ["error", "never"],
+        "jsonc/indent": ["error", 2],
+        // Enforce consistent spacing between keys and values in object literal properties
+        "jsonc/key-spacing": ["error", { multiline: true, afterColon: true }],
+        // Disallow octal escape sequences in string literals
+        "jsonc/no-octal-escape": "error",
+        // Enforce consistent line breaks inside braces
+        "jsonc/object-curly-newline": ["error", { multiline: true, consistent: true }],
+        // Enforce consistent spacing inside braces
+        "jsonc/object-curly-spacing": ["error", "always"],
+        // Enforce placing object properties on separate lines.
+        "jsonc/object-property-newline": ["error", { allowMultiplePropertiesPerLine: true }],
+      }
+    },
+    {
+      files: ["*.yaml", "*.yml"],
+      parser: "yaml-eslint-parser",
+      rules: {
+        // Enforce consistent spacing after the # in a comment. Off
+        "spaced-comment": "off"
+      }
+    },
+    {
+      files: ["package.json"],
+      parser: "jsonc-eslint-parser",
+      rules: {
+        // Require object keys to be sorted
+        "jsonc/sort-keys": [
+          "error",
+          {
+            pathPattern: "^$",
+            order: [
+              'publisher',
+              'name',
+              'displayName',
+              'type',
+              'version',
+              'private',
+              'packageManager',
+              'description',
+              'author',
+              'license',
+              'funding',
+              'homepage',
+              'repository',
+              'bugs',
+              'keywords',
+              'categories',
+              'sideEffects',
+              'exports',
+              'main',
+              'module',
+              'unpkg',
+              'jsdelivr',
+              'types',
+              'typesVersions',
+              'bin',
+              'icon',
+              'files',
+              'engines',
+              'activationEvents',
+              'contributes',
+              'scripts',
+              'peerDependencies',
+              'peerDependenciesMeta',
+              'dependencies',
+              'optionalDependencies',
+              'devDependencies',
+              'pnpm',
+              'overrides',
+              'resolutions',
+              'husky',
+              'simple-git-hooks',
+              'lint-staged',
+              'eslintConfig',
+            ]
+          },
+          {
+            pathPattern: "^(?:dev|peer|optional|bundled)?[Dd]dependencies$",
+            order: { type: "asc" }
+          },
+          {
+            pathPattern: "^exports.*$",
+            order: [
+              "types",
+              "require",
+              "import"
+            ]
+          }
+        ]
+      }
+    },
+    {
+      files: ["*.d.ts"],
+      rules: {
+        "import/no-duplicates": "off"
+      }
+    },
+    {
+      files: ["*.ts", "*.tsx", "*.mts", "*cts"],
+      rules: {
+        "no-void": ["error", { allowAsStatement: true }]
+      }
+    },
+    {
+      files: ["scripts/**/*.*", "cli.*"],
+      rules: {
+        "no-console": "off"
+      }
+    },
+    {
+      files: ["*.test.ts", "*.test.js", "*.spec.ts", "*.spec.js"],
+      rules: {
+        "no-unused-expressions": "off",
+        "no-only-tests/no-only-tests": "error"
+      }
+    },
+    {
+      // Code blocks in markdown file
+      files: ["**/*.md/*.*"],
+      rules: {
+        "@typescript-eslint/no-redeclare": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-use-before-define": "off",
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/comma-dangle": "off",
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-require-imports": "off",
+        "import/no-unresolved": "off",
+        "unused-imports/no-unused-imports": "off",
+        "unused-imports/no-unused-vars": "off",
+        "no-alert": "off",
+        "no-console": "off",
+        "no-restricted-imports": "off",
+        "no-undef": "off",
+        "no-unused-expressions": "off",
+        "no-unused-vars": "off",
+      }
+    }
+  ],
+  rules: {
+    // 与 --report-unused-disable-directives 指令具有相同的效果，但相对更有用，因为可以在共享配置中配置。
+    // "eslint-comments/no-unused-disable": "error",
+
+    // import
+    "import/order": "error",
+    "import/first": "error",
+    "import/no-mutable-exports": "error",
+    "import/no-unresolved": "off",
+    "import/no-absolute-path": "off",
+    "import/newline-after-import": ["error", { count: 1, considerComments: true }],
+
+    // Common
+    "semi": ["error", "never"],
+    "curly": ["error", "multi-or-nest", "consistent"],
+    "quotes": ["error", "single"],
+    "quote-props": ["error", "consistent-as-needed"],
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_"
+      }
+    ],
+    "no-param-reassign": "off",
+    "array-bracket-spacing": ["error", "never"],
+    "brace-style": ["error", "stroustrup", { allowSingleLine: true }],
+    "block-spacing": ["error", "always"],
+    "camelcase": "off",
+    "comma-spacing": ["error", { before: false, "after": true }], "comma-style": ["error", "last"],
+    "comma-dangle": ["error", "always-multiline"],
+    "no-constant-condition": "warn",
+    "no-debugger": "error",
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-cond-assign": ["error", "always"],
+    "func-call-spacing": "off",
+    "key-spacing": ["error", { beforeColon: false, afterColon: true }],
+    "indent": ["error", 2, { SwitchCase: 1, VariableDeclarator: 1, outerIIFEBody: 1 }],
+    "no-restricted-syntax": ["error", "DebuggerStatement", "LabeledStatement", "WithStatement"],
+    "object-curly-spacing": ["error", "always"],
+    "no-return-await": "off",
+    "space-before-function-paren": [
+      "error",
+      {
+        anonymous: "always",
+        named: "never",
+        asyncArrow: "always"
+      }
+    ],
+    "no-restricted-globals": [
+      "error",
+      { name: "global", message: "Use `globalThis` instead." },
+      { name: "self", message: "Use `globalThis` instead." }
+    ],
+    "no-restricted-properties": [
+      "error",
+      {
+        property: "__proto__",
+        message: "Use `Object.getPrototypeOf` or `Object.setPrototypeOf` instead."
+      },
+      {
+        property: "__defineGetter__",
+        message: "Use `Object.defineProperty` instead."
+      },
+      {
+        property: "__defineSetter__",
+        message: "Use `Object.defineProperty` instead."
+      },
+      {
+        property: "__lookupGetter__",
+        message: "Use `Object.getOwnPropertyDescriptor` instead."
+      },
+      {
+        property: "__lookupSetter__",
+        message: "Use `Object.getOwnPropertyDescriptor` instead."
+      }
+    ],
+
+    // es6
+    "no-var": "error",
+    "prefer-const": [
+      "error",
+      {
+        destructuring: "all",
+        ignoreReadBeforeAssign: true
+      }
+    ],
+    "prefer-arrow-callback": [
+      "error",
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true
+      }
+    ],
+    "object-shorthand": [
+      "error",
+      "always",
+      {
+        ignoreConstructors: false,
+        avoidQuotes: true
+      }
+    ],
+    "prefer-exponentiation-operator": "error",
+    "prefer-rest-params": "error",
+    "prefer-spread": "error",
+    "prefer-template": "error",
+    "template-curly-spacing": "error",
+    "arrow-parens": [
+      "error",
+      "as-needed",
+      {
+        requireForBlockBody: true
+      }
+    ],
+    "generator-star-spacing": "off",
+    "spaced-comment": [
+      "error",
+      "always",
+      {
+        line: { markers: ["/"], exceptions: ["/", "#"] },
+        block: { markers: ["!"], exceptions: ["*"], balanced: true }
+      }
+    ],
+
+    // best-practice
+    "array-callback-return": "error",
+    "block-scoped-var": "error",
+    "consistent-return": "off",
+    "complexity": "off",
+    "eqeqeq": ["error", "smart"],
+    "no-alert": "warn",
+    "no-case-declarations": "error",
+    "no-multi-spaces": "error",
+    "no-multi-str": "error",
+    "no-with": "error",
+    "no-void": "error",
+    "no-useless-escape": "off",
+    "no-invalid-this": "error",
+    "vars-on-top": "error",
+    "require-await": "off",
+    "no-return-assign": "off",
+    "operator-linebreak": ["error", "before"],
+    "max-statements-per-line": ["error", { max: 1 }],
+
+    // node
+    "n/prefer-global/buffer": ["error", "never"],
+    "n/no-callback-literal": "off",
+
+    // unicorn
+    "unicorn/error-message": "error",
+    "unicorn/escape-case": "error",
+    "unicorn/no-instanceof-array": "error",
+    "unicorn/no-new-buffer": "error",
+    "unicorn/no-unsafe-regex": "off",
+    "unicorn/number-literal-case": "error",
+    "unicorn/prefer-includes": "error",
+    "unicorn/prefer-string-starts-ends-with": "error",
+    "unicorn/prefer-text-content": "error",
+    "unicorn/prefer-type-error": "error",
+    "unicorn/throw-new-error": "error",
+    "unicorn/prefer-node-protocol": "error",
+    "unicorn/prefer-number-properties": "error",
+
+
+    "no-use-before-define": ["error", { "functions": false, "classes": false, "variables": true }],
+    "eslint-comments/disable-enable-pair": "off",
+    "import/no-named-as-default-member": "off",
+    "import/no-named-as-default": "off",
+    "import/namespace": "off",
+
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: false
+      }
+    ],
+
+    // yml
+    "yml/quotes": [
+      "error",
+      {
+        prefer: "single",
+        avoidEscape: false
+      }
+    ],
+    "yml/no-empty-document": "off",
+  }
+};
